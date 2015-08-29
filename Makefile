0bbd07b23952f327d14c700a588a9083e1941e6d
Andrew Clayton <andrew@digital-domain.net>
2015-08-29 12:40:34 +0100

    ipv6-isin: Determine if an address is in a given network
    Initial commit of this utility. Run like
    
        ./ipv6-isin 2001:db8:abcd:1234::/64 2001:db8:abcd:1234::cafe
        Yes
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -std=c99 -g -O2
 
-all: ipv6-range ipv6-arpa
+all: ipv6-range ipv6-arpa ipv6-isin
 
 ipv6-range: ipv6-range.c
 	$(CC) $(CFLAGS) -o ipv6-range ipv6-range.c
@@ -9,5 +9,8 @@ ipv6-range: ipv6-range.c
 ipv6-arpa: ipv6-arpa.c
 	$(CC) $(CFLAGS) -o ipv6-arpa ipv6-arpa.c
 
+ipv6-isin: ipv6-isin.c
+	$(CC) $(CFLAGS) -o ipv6-isin ipv6-isin.c
+
 clean:
-	rm -f ipv6-range ipv6-arpa
+	rm -f ipv6-range ipv6-arpa ipv6-isin
---
+++ ipv6-isin.c
@@ -0,0 +1,66 @@
+/*
+ * ipv6-isin.c - Given an IPv6 network/prefix and an IPv6 address, determine
+ * 		 if the address falls within the given network.
+ *
+ *  Copyright (C) 2015	Andrew Clayton <andrew@digital-domain.net>
+ *
+ *  Licensed under the GNU General Public License Version 2 or
+ *  the GNU Lesser General Public License Version 2.1
+ *
+ *  See GPLv2 & LGPLv2.1 in the source tree.
+ */
+
+#define _DEFAULT_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+/*
+ * Function based on the tcp wrappers IPv6 code from
+ * Casper Dik (Casper.Dik@Holland.Sun.COM)
+ */
+static void ipv6_isin(const char *network, unsigned short prefixlen,
+		      const char *address)
+{
+	unsigned char ip6b[sizeof(struct in6_addr)];
+	unsigned char *p = ip6b;
+	char ip6s[INET6_ADDRSTRLEN];
+
+	inet_pton(AF_INET6, address, ip6b);
+
+	p += prefixlen / 8;
+	prefixlen %= 8;
+
+	if (prefixlen !=  0)
+		*p &= 0xff << (8 - prefixlen);
+
+	while (p < ip6b + sizeof(ip6b))
+		*p++ = 0;
+
+	inet_ntop(AF_INET6, ip6b, ip6s, INET6_ADDRSTRLEN);
+	if (strcmp(network, ip6s) == 0)
+		printf("Yes\n");
+	else
+		printf("No\n");
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned short prefixlen;
+	char *ptr;
+
+	if (argc < 3) {
+		printf("Usage: ipv6-isin network/prefix address\n");
+		exit(EXIT_FAILURE);
+	}
+
+	prefixlen = atoi(strchr(argv[1], '/') + 1);
+	ptr = strchr(argv[1], '/');
+	*ptr = '\0';
+	ipv6_isin(argv[1], prefixlen, argv[2]);
+
+	exit(EXIT_SUCCESS);
+}

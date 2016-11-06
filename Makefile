3904af4605737695bebbad608b35fa3385847bf3
Andrew Clayton <andrew@digital-domain.net>
2016-11-06 19:06:42 +0000

    gen-ula: Add a tool to generate IPv6 ULA prefix's
    This utility generates IPv6 Unique Local Address prefix's.
    
    These perform an approximate job as RFC 1918 private IPv4 addresses.
    
    It produces prefix's in the fd00::/8 space. e.g fd24:28d2:e8a1::/48. It
    also prints out the /64 subnet range for the generated /48, e.g
    
        $ ./gen-ula
        fd0c:d0e3:46f0::/48
        /64 subnet range:
            fd0c:d0e3:46f0:0000::/64 -
            fd0c:d0e3:46f0:ffff::/64
    
    It produces the prefix using an algorithm based on that in RFC 4193[0]
    
    [0] - https://tools.ietf.org/html/rfc4193#section-3.2.2
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -std=c99 -g -O2
 
-all: ipv6-range ipv6-arpa ipv6-isin
+all: ipv6-range ipv6-arpa ipv6-isin gen-ula
 
 ipv6-range: ipv6-range.c
 	$(CC) $(CFLAGS) -o ipv6-range ipv6-range.c
@@ -12,5 +12,8 @@ ipv6-arpa: ipv6-arpa.c
 ipv6-isin: ipv6-isin.c
 	$(CC) $(CFLAGS) -o ipv6-isin ipv6-isin.c
 
+gen-ula: gen-ula.c
+	$(CC) $(CFLAGS) -o gen-ula gen-ula.c -luuid -lmhash
+
 clean:
-	rm -f ipv6-range ipv6-arpa ipv6-isin
+	rm -f ipv6-range ipv6-arpa ipv6-isin gen-ula
---
+++ gen-ula.c
@@ -0,0 +1,63 @@
+/*
+ * gen-ula.c -	Generate an IPv6 Unique Local Address prefix
+ *
+ * Copyright (C) 2016		Andrew Clayton <andrew@digital-domain.net>
+ *
+ * Licensed under the GNU General Public License Version 2 or
+ * the GNU Lesser General Public License Version 2.1
+ *
+ * See GPLv2 & LGPLv2.1 in the source tree.
+ */
+
+#define _POSIX_C_SOURCE		199309L	/* clock_gettime(2) */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <uuid/uuid.h>
+
+#include <mhash.h>
+
+int main(void)
+{
+	struct timespec tp;
+	uuid_t uuid;
+	char uuid_s[37];
+	char key[128];
+	char ula[15] = "fd";		/* fdxx:xxxx:xxxx + \0 */
+	unsigned char *xhash;
+	MHASH td;
+	int len;
+	int i;
+
+	/* Generate an IPv6 ULA prefix based on the algorithm from RFC 4193 */
+	uuid_generate(uuid);
+	uuid_unparse(uuid, uuid_s);
+	clock_gettime(CLOCK_REALTIME, &tp);
+	len = snprintf(key, sizeof(key), "%s%ld%ld", uuid_s, tp.tv_sec,
+			tp.tv_nsec);
+
+	td = mhash_init(MHASH_SHA1);
+	mhash(td, key, len);
+	xhash = mhash_end(td);
+
+	len = 2;
+	/* Use lower 40 bits (5 bytes) */
+	for (i = 19; i > 14; i--) {
+		if (i % 2 == 0) {
+			strcat(ula, ":");
+			len++;
+		}
+		len += snprintf(ula + len, sizeof(ula), "%.2x", xhash[i]);
+	}
+	free(xhash);
+
+	printf("%s::/48\n", ula);
+	printf("/64 subnet range:\n");
+	printf("    %s:0000::/64 -\n", ula);
+	printf("    %s:ffff::/64\n", ula);
+
+	exit(EXIT_SUCCESS);
+}

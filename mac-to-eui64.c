f84388861e9d4ee6eb6d82c2e2790ec4a17eaa60
Andrew Clayton <andrew@digital-domain.net>
2016-12-02 19:12:06 +0000

    mac-to-eui64: Use uint8_t for the MAC array
    This is a much better match than 'long'.
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- mac-to-eui64.c
+++ mac-to-eui64.c
@@ -12,10 +12,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
-	long mac[8];
+	uint8_t mac[8];
 	int i;
 
 	if (argc < 2) {
@@ -23,20 +24,20 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	mac[0] = strtol(argv[1], NULL, 16);
-	mac[1] = strtol(argv[1]+3, NULL, 16);
-	mac[2] = strtol(argv[1]+6, NULL, 16);
+	mac[0] = strtoul(argv[1], NULL, 16);
+	mac[1] = strtoul(argv[1]+3, NULL, 16);
+	mac[2] = strtoul(argv[1]+6, NULL, 16);
 	mac[3] = 0xff;
 	mac[4] = 0xfe;
-	mac[5] = strtol(argv[1]+9, NULL, 16);
-	mac[6] = strtol(argv[1]+12, NULL, 16);
-	mac[7] = strtol(argv[1]+15, NULL, 16);
+	mac[5] = strtoul(argv[1]+9, NULL, 16);
+	mac[6] = strtoul(argv[1]+12, NULL, 16);
+	mac[7] = strtoul(argv[1]+15, NULL, 16);
 
 	/* Toggle the (U/L) bit 0000 00*0 */
 	mac[0] ^= 1 << 1;
 
 	for (i = 0; i < 8; i++)
-		printf("%02lx%s", mac[i], (i < 7) ? ":" : "");
+		printf("%02x%s", mac[i], (i < 7) ? ":" : "");
 	printf("\n");
 
 	exit(EXIT_SUCCESS);

fd60d0235a2784fed0f6fb4355b168c802c26fb7
Andrew Clayton <andrew@digital-domain.net>
2017-02-27 12:35:01 +0000

    mac-to-eui64: Also display in groups of 2 octets
    Extend the output a bit to also show the SLAAC host part in the right
    format. e.g
    
        ./mac-to-eui64 00:21:2f:b5:6e:10
        Modified EUI-64 :-
                     02:21:2f:ff:fe:b5:6e:10
        (SLAAC Host) ::0221:2fff:feb5:6e10
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- mac-to-eui64.c
+++ mac-to-eui64.c
@@ -2,7 +2,7 @@
  * mac-to-eui64.c - Generate a modified EUI-64 identifier for the given MAC
  * 		    address.
  *
- * Copyright (C) 2016		Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2016 - 2017	Andrew Clayton <andrew@digital-domain.net>
  *
  * Licensed under the GNU General Public License Version 2 or
  * the GNU Lesser General Public License Version 2.1
@@ -36,8 +36,12 @@ int main(int argc, char *argv[])
 	/* Toggle the (U/L) bit 0000 00*0 */
 	mac[0] ^= 1 << 1;
 
+	printf("Modified EUI-64 :-\n             ");
 	for (i = 0; i < 8; i++)
 		printf("%02x%s", mac[i], (i < 7) ? ":" : "");
+	printf("\n(SLAAC Host) ::");
+	for (i = 0; i < 8; i += 2)
+		printf("%02x%02x%s", mac[i], mac[i + 1], i+1 < 7 ? ":" : "");
 	printf("\n");
 
 	exit(EXIT_SUCCESS);

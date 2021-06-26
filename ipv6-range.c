deb6eff9910b016831284d2ec395be5b88ecaf38
Andrew Clayton <andrew@digital-domain.net>
2021-06-26 01:00:26 +0100

    ipv6-range: Reduce some code duplication
    Factor out the printing of the start and end addresses as this is
    exactly the same for the two.
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- ipv6-range.c
+++ ipv6-range.c
@@ -2,8 +2,8 @@
  * ipv6-range.c - Given an IPv6 network address with prefix, calculate the
  * 		  range of IPs available.
  *
- *  Copyright (C) 2015 - 2017, 2020	Andrew Clayton
- *					<andrew@digital-domain.net>
+ *  Copyright (C) 2015 - 2017, 2020 - 2021	Andrew Clayton
+ *						<andrew@digital-domain.net>
  *
  *  Licensed under the GNU General Public License Version 2 or
  *  the GNU Lesser General Public License Version 2.1
@@ -25,6 +25,30 @@
 #define HIGH_NIBBLE(byte) (((byte) >> 4) & 0x0f)
 #define LOW_NIBBLE(byte)  ((byte) & 0x0f)
 
+static void pretty_print_address(const struct in6_addr *in6, u8 prefixlen)
+{
+	const char *txtfmt = "";
+
+	for (int i = 0; i < 15; i += 2) {
+		if ((i*8) + 4 > prefixlen)
+			txtfmt = TXT_FMT_BOLD;
+		printf("%s%x", txtfmt, HIGH_NIBBLE(in6->s6_addr[i]));
+		if ((i*8) + 8 > prefixlen)
+			txtfmt = TXT_FMT_BOLD;
+		printf("%s%x", txtfmt, LOW_NIBBLE(in6->s6_addr[i]));
+		if (((i+1) * 8) + 4 > prefixlen)
+			txtfmt = TXT_FMT_BOLD;
+		printf("%s%x", txtfmt, HIGH_NIBBLE(in6->s6_addr[i + 1]));
+		if (((i+1) * 8) + 8 > prefixlen)
+			txtfmt = TXT_FMT_BOLD;
+		printf("%s%x", txtfmt, LOW_NIBBLE(in6->s6_addr[i + 1]));
+
+		if (i < 14)
+			printf(":");
+	}
+	printf(TXT_FMT_END"\n");
+}
+
 /*
  * Based on code from https://github.com/peczenyj/IPv6SubnetCalc/
  */
@@ -37,7 +61,6 @@ static void ipv6_range(const char *network, u8 prefixlen)
 	int len = 0;
 	int imask = 128 - prefixlen;
 	char net_s[128] = "\0";
-	char *txtfmt = "";
 
 	inet_pton(AF_INET6, network, &ip6b);
 
@@ -76,47 +99,11 @@ static void ipv6_range(const char *network, u8 prefixlen)
 				((u64)UINT64_MAX >> prefixlen) + 1);
 	net_s[len - 2] = '\0';
 
-	printf("Network : %s/%u\n\t  (%s)\n", network, prefixlen,
-	       net_s);
+	printf("Network : %s/%u\n\t  (%s)\n", network, prefixlen, net_s);
 	printf("Start   : ");
-	for (i = 0; i < 15; i += 2) {
-		if ((i*8) + 4 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, HIGH_NIBBLE(ip6sb.s6_addr[i]));
-		if ((i*8) + 8 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, LOW_NIBBLE(ip6sb.s6_addr[i]));
-		if (((i+1) * 8) + 4 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, HIGH_NIBBLE(ip6sb.s6_addr[i + 1]));
-		if (((i+1) * 8) + 8 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, LOW_NIBBLE(ip6sb.s6_addr[i + 1]));
-
-		if (i < 14)
-			printf(":");
-	}
-	printf(TXT_FMT_END"\n");
-	txtfmt = "";
-
+	pretty_print_address(&ip6sb, prefixlen);
 	printf("End     : ");
-	for (i = 0; i < 15; i += 2) {
-		if ((i*8) + 4 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, HIGH_NIBBLE(ip6eb.s6_addr[i]));
-		if ((i*8) + 8 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, LOW_NIBBLE(ip6eb.s6_addr[i]));
-		if (((i+1) * 8) + 4 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, HIGH_NIBBLE(ip6eb.s6_addr[i + 1]));
-		if (((i+1) * 8) + 8 > prefixlen)
-			txtfmt = TXT_FMT_BOLD;
-		printf("%s%x", txtfmt, LOW_NIBBLE(ip6eb.s6_addr[i + 1]));
-		if (i < 14)
-			printf(":");
-	}
-	printf(TXT_FMT_END"\n");
+	pretty_print_address(&ip6eb, prefixlen);
 }
 
 int main(int argc, char *argv[])

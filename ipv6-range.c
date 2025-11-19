ac000/ipv6-tools

commit cf4f1a1af54df8c7966c3a70f6e73a6f9c6661be

ipv6-range: Remove the network/prefix argument check
Remove the check on the network/prefix that basically required it to end
in ::/n as this disallowed such things as 2001:db8::cafe:0/112 which is
perfectly valid.

Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

ipv6-range.c
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
 	u8 prefixlen;
 	char *ptr;
 
-	if (argc < 2 || !strstr(argv[1], "::/")) {
+	if (argc < 2) {
 		printf("Usage: ipv6-range network/prefix\n");
 		exit(EXIT_FAILURE);
 	}

commit 43bac29773d4c8283cc0686644937e1323539939

ipv6-range: Don't crash when no prefix is specified
Fixes: cf4f1a1 ("ipv6-range: Remove the network/prefix argument check")
Signed-off-by: Andrew Clayton <ac@sigsegv.uk>

ipv6-range.c
@@ -2,7 +2,7 @@
  * ipv6-range.c - Given an IPv6 network address with prefix, calculate the
  * 		  range of IPs available.
  *
- *  Copyright (C) 2015 - 2025	Andrew Clayton <andrew@digital-domain.net>
+ *  Copyright (C) 2015 - 2025	Andrew Clayton <ac@sigsegv.uk>
  *
  *  Licensed under the GNU General Public License Version 2 or
  *  the GNU Lesser General Public License Version 2.1
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
 	u8 prefixlen;
 	char *ptr;
 
-	if (argc < 2) {
+	if (argc < 2 || !strchr(argv[1], '/')) {
 		printf("Usage: ipv6-range network/prefix\n");
 		exit(EXIT_FAILURE);
 	}